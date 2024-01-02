376b0116db7b8b90b0f4286388c212ba1a520fbf
Josef Gabrielsson <josef.gabrielsson@gu.se>
2024-01-02 21:19:57 +0300

    .

--- front/modules/math.js
+++ front/modules/math.js
@@ -6,8 +6,19 @@ app.module.math = {
     app.element.set(element, Math.round(parseFloat(value)))
   },
 
+  alert: function(value) {
+    alert('yes')
+  },
+
   compute: function(element, value) {
-    var value = app.element.get(element).replace(/([=+\-*/])(?=[=+\-*/])/g, '')
+    var value = app.element.get(element).replace(/[^0-9+\-*/.()]/g, '')
+    // Replace instances of digits followed by an opening parenthesis with the multiplication sign
+    value = value.replace(/(\d)\(/g, '$1*(')
+
+    // Replace commas with periods
+    value = value.replace(/,/g, '.')
+
+    // Evaluate the expression
     app.element.set(element, eval(value))
   }
 }
